72627c8ad373edb359beb95cee3c28563e2d261e
Fabio Penna <ffmpenna@gmail.com>
2022-06-22 20:54:50 -0300

    add resolução do exercício 2-2

---
+++ 1_fundamentos/bloco_4_introducao_a_javascript_e_logica_de_programacao/dia-2-javascript-array-e-loop-for/Exercicio 2-2.js
@@ -0,0 +1,19 @@
+const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let soma = 0;
+
+for (let valores of numbers){
+    soma = soma + valores;
+}
+
+console.log(soma);
+
+// let sum = 0;
+
+// for (let index = 0; index < numbers.length; index += 1){
+
+//     sum = sum + numbers[index]
+
+// }
+
+// console.log(sum);
