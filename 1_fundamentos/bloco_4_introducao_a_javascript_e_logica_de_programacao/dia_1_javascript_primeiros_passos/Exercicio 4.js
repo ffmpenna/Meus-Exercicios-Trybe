7d019a7b9b24299660281eec854fb0753cfc16d3
Fabio Penna <ffmpenna@gmail.com>
2022-06-21 22:35:37 -0300

    add resolucao do exercicio 4

---
+++ 1_fundamentos/bloco_4_introducao_a_javascript_e_logica_de_programacao/dia_1_javascript_primeiros_passos/Exercicio 4.js
@@ -0,0 +1,11 @@
+const numero = 5 - 3 * 2;
+
+if (numero > 0){
+    console.log('positive (', numero, ')')
+}
+else if (numero < 0){
+    console.log('negative (', numero, ')')
+}
+else{
+    console.log('zero (', numero, ')')
+}
